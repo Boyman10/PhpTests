29bf5ddb8151ab0540e70d48d9af1d526600ac33
Eddy Mio <contact@remotehireman.fr>
2018-02-01 18:08:37 +0100

    testing generator and references

--- generator.php
+++ generator.php
@@ -51,3 +51,51 @@ foreach (generator() as $key => $val)
 {
     echo $key, ' => ', $val, '<br />';
 }
+
+
+/**
+ *
+ * Now using Class and references
+ */
+  
+class SomeClass
+{
+    protected $attr;
+    
+    public function __construct()
+    {
+        $this->attr = ['Un', 'Deux', 'Trois', 'Quatre'];
+    }
+    
+    // Le & avant le nom du générateur indique que les valeurs retournées sont des références
+    public function &generator()
+    {
+        // On cherche ici à obtenir les références des valeurs du tableau pour les retourner
+        foreach ($this->attr as &$val)
+        {
+            yield $val;
+        }
+    }
+    
+    public function attr()
+    {
+        return $this->attr;
+    }
+}
+
+$obj = new SomeClass;
+
+// On parcourt notre générateur en récupérant les entrées par référence
+foreach ($obj->generator() as &$val)
+{
+    // On effectue une opération quelconque sur notre valeur
+    $val = strrev($val);
+}
+
+echo '<pre>';
+var_dump($obj->attr());
+echo '</pre>';
+
+
+
+
