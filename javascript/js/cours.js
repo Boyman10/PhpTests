6df9d1d16481191e898feacdecddb78cd89e4200
Boyman10 <contact@yasi.fr>
2018-02-03 17:33:56 +0100

    Adding Ajax function

--- javascript/js/cours.js
+++ javascript/js/cours.js
@@ -22,4 +22,38 @@ console.log(h1.parentNode); // Affiche le noeud body
 
 console.log(document.parentNode); // Affiche null : document n'a aucun noeud parent
 
+/**
+ * Query Selectors : slower !!
+ */
 
+//Tous les paragraphes
+console.log(document.querySelectorAll("p").length); // Affiche 3
+
+// Tous les paragraphes à l'intérieur de l'élément identifié par "contenu"
+console.log(document.querySelectorAll("#contenu p").length); // Affiche 2
+
+// Tous les éléments ayant la classe "existe"
+console.log(document.querySelectorAll(".existe").length); // Affiche 8
+
+//L'attribut href du premier lien
+console.log(document.querySelector("a").getAttribute("href"));
+
+
+//Liste des classes de l'élément identifié par "antiques"
+var classes = document.getElementById("antiques").classList;
+console.log(classes.length); // Affiche 1 : l'élément possède une seule classe
+console.log(classes[0]); // Affiche "merveilles"
+
+
+if (document.getElementById("antiques").classList.contains("merveille")) {
+    console.log("L'élément identifié par antiques possède la classe merveille");
+} else {
+    console.log("L'élément identifié par antiques ne possède pas la classe merveille");
+}
+
+
+//Ajout d'un gestionnaire qui affiche le type et la cible de l'événement
+document.addEventListener("click", function (e) {
+    console.log("Evènement : " + e.type + 
+        ", texte de la cible : " + e.target.textContent);
+});
