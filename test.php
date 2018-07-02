76b09261fe717c01b9fe79e7926e4880256a1941
Eddy Mio <contact@remotehireman.fr>
2018-07-02 19:00:35 +0200

    Testing call_user_func_array :)

--- test.php
+++ test.php
@@ -1,5 +1,15 @@
 <?php 
 
+class A {
+    
+    public function meNow($a,$b)
+    {
+        var_dump($a);
+        var_dump($b);
+    }
+}
+
+
 $str = "0000040";
 
 echo (int) $str;
@@ -12,3 +22,21 @@ $paths2 = array_filter($paths,is_string);
 var_dump($paths);
 
 var_dump($paths2);
+
+$params=$_GET['params'];
+
+print_r($params);
+
+
+// testing clall_user_func_array();
+$class = "A";
+$target = "meNow";
+
+if (class_exists($class, true)) {
+    $class = new $class();
+    if (in_array($target, get_class_methods($class))) {
+        //Appel de la méthode $class->target() avec tableau arguments
+        call_user_func_array([$class, $target], $params);
+    }
+}
+
