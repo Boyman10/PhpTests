05c813d17345c9d1749b34f8a38da40901bbfaba
Boyman10 <contact@yasi.fr>
2018-07-05 22:34:28 +0200

    Working on animate

---
+++ javascript/js/coord.js
@@ -0,0 +1,20 @@
+function degreesToRadians(degrees) {
+  return degrees * Math.PI / 180;
+}
+
+function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
+  var earthRadiusKm = 6371;
+
+  var dLat = degreesToRadians(lat2-lat1);
+  var dLon = degreesToRadians(lon2-lon1);
+
+  lat1 = degreesToRadians(lat1);
+  lat2 = degreesToRadians(lat2);
+
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  return earthRadiusKm * c;
+}
+
+alert(distanceInKmBetweenEarthCoordinates(43.608177, 3.879483, 43.6071285339217, 3.87952263361082));
